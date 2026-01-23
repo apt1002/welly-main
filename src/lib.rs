7b211a2a5efee91ebf5abb002dc46521a801faab
Alistair Turnbull <github@minworks.co.uk>
2026-01-23 00:12:11 +0000

    Export some useful stuff.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 //! A top-level for the Welly programming language. This crate provides error
 //! reporting, including source code locations. It also provides a REPL.
 
+pub use ansi_term;
+
 mod loc;
 pub use loc::{Location, Loc, Locate};
 
 mod error;
-pub use error::{Error, Result};
+pub use error::{Report, Error, Result};
 
 mod repl;
 pub use repl::{Repl};
